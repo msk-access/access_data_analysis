a5baba408f4d73d465c9dcc1f3c36e28844239af
Youyun Zheng <zhengy1@juno.cbio.private>
2020-05-11 21:36:24 -0400

    finished with step 4 (CNA calling) and 5 (plotting). need to add annotation and readme in the future

--- R/SV_incorporation.R
+++ R/SV_incorporation.R
@@ -134,7 +134,7 @@ if (!interactive()) {
   
   master.ref = args$masterref
   results.dir = args$resultsdir
-  dmp.key.path = args$dmpdir
+  dmpdir = args$dmpdir
   criteria = args$criteria
   
   cat(paste0(paste0(c(paste0(rep('-',15),collapse = ''),'Arguments input: ',master.ref,results.dir,dmpdir,criteria,
@@ -144,7 +144,7 @@ if (!interactive()) {
     stop('Criteria argument should be either stringent or permissive')
   }
   
-  suppressWarnings(filter_calls(fread(master.ref),results.dir,dmpdir,criteria))
+  suppressWarnings(SV_incorporation(fread(master.ref),results.dir,dmpdir,criteria))
   
 }
 
