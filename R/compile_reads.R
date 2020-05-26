677b99e781d0cd1daae8e6051e590f2e9f2dbcc4
Youyun Zheng <zhengy1@juno.cbio.private>
2020-05-26 11:29:44 -0400

    changes to step 1 script to address issue 2 and 4

--- R/compile_reads.R
+++ R/compile_reads.R
@@ -6,7 +6,7 @@
 
 #' @export
 compile_reads = function(
-  master.ref,results.dir,pooled.bam.dir = '/ifs/work/bergerm1/ACCESS-Projects/novaseq_curated_duplex_v2/',
+  master.ref,results.dir,project.ID,pooled.bam.dir = '/ifs/work/bergerm1/ACCESS-Projects/novaseq_curated_duplex_v2/',
   fasta.path = '/work/access/production/resources/reference/current/Homo_sapiens_assembly19.fasta',
   genotyper.path = '/ifs/work/bergerm1/Innovation/software/maysun/GetBaseCountsMultiSample/GetBaseCountsMultiSample',
   dmp.dir = '/ifs/work/bergerm1/zhengy1/dmp/mskimpact/',mirror.bam.dir = '/ifs/dmpshare/share/irb12_245/',
@@ -33,9 +33,15 @@ compile_reads = function(
   }
   
   # data from DMP -----------------------------------------------------------
-  DMP.maf = fread(paste0(dmp.dir,'/data_mutations_extended.txt')) %>% filter(Mutation_Status != 'GERMLINE') %>% data.table()
   DMP.key = fread(dmp.key.path)
-  DMP.RET.maf = DMP.maf[grepl(paste0(unique(master.ref[!is.na(dmp_patient_id)]$dmp_patient_id),collapse = '|'),Tumor_Sample_Barcode),]
+  if(any(!master.ref[grepl('^P-',dmp_patient_id)]$dmp_patient_id %in% gsub('-T..-IM.','',DMP.key$V1))){
+    stop(paste0(
+      'These DMP IDs are not found in DMP key file: ',
+      paste0(master.ref[grepl('^P-',dmp_patient_id)]$dmp_patient_id[which(!master.ref[grepl('^P-',dmp_patient_id)]$dmp_patient_id %in% gsub('-T..-IM.','',DMP.key$V1))],collapse = ' ,')
+    ))
+  }
+  DMP.maf = fread(paste0(dmp.dir,'/data_mutations_extended.txt')) %>% filter(Mutation_Status != 'GERMLINE') %>% data.table()
+  DMP.RET.maf = DMP.maf[grepl(paste0(unique(master.ref[grepl('^P-',dmp_patient_id)]$dmp_patient_id),collapse = '|'),Tumor_Sample_Barcode),]
   
   # Pooled normal samples ---------------------------------------------------
   pooled.bams = list.files(pooled.bam.dir,pattern = '.bam',full.names = T)
@@ -44,6 +50,7 @@ compile_reads = function(
   x = unique(master.ref$cmo_patient_id)[1]
   # x = unique(master.ref$cmo_sample_id_plasma)[16]
   # x = 'C-YW82CY'
+  print('Compiling reads per patient')
   all.fillout.id = lapply(unique(master.ref$cmo_patient_id),function(x){
     print(x)
     dir.create(paste0(results.dir,'/',x))
@@ -70,6 +77,7 @@ compile_reads = function(
     # piece together all unique calls -----------------------------------------
     # get duplex calls
     duplex.calls = do.call(rbind,lapply(master.ref[cmo_patient_id == x]$maf_path,function(x){
+      # fread(x) %>% filter(as.numeric(D_t_alt_count_fragment) > 0) %>% data.table()
       fread(x) %>% filter(as.numeric(t_alt_count) > 0) %>% data.table()
     }))
     # get impact calls
@@ -77,7 +85,8 @@ compile_reads = function(
     write.table(impact.calls[,.(Hugo_Symbol,Chromosome,Start_Position,End_Position,Variant_Classification,HGVSp_Short,Reference_Allele,Tumor_Seq_Allele2)],
                 paste0(results.dir,'/',x,'/',x,'_impact_calls.maf'),sep = '\t',quote = F,row.names = F)
     # combining plasma and impact calls
-    all.calls = rbind(duplex.calls[,intersect(colnames(duplex.calls),colnames(DMP.RET.maf)),with = F],impact.calls[,intersect(colnames(duplex.calls),colnames(DMP.RET.maf)),with = F])
+    all.calls = rbind(duplex.calls[,intersect(colnames(duplex.calls),colnames(DMP.RET.maf)),with = F],
+                      impact.calls[,intersect(colnames(duplex.calls),colnames(DMP.RET.maf)),with = F])
     # getting rid of duplicate calls and take the first occurence of all events
     all.calls = all.calls[which(!duplicated(all.calls[,.(Hugo_Symbol,Chromosome,Start_Position,End_Position,Variant_Classification,HGVSp_Short,Reference_Allele,Tumor_Seq_Allele2)])),] %>%
       filter(Variant_Classification != 'Silent' & !grepl('RP11-',Hugo_Symbol) & !grepl('Intron',Variant_Classification))
@@ -85,6 +94,7 @@ compile_reads = function(
     # tagging hotspots
     system(paste0(
       'bsub  -R "rusage[mem=4]" -cwd ',results.dir,'/',x,'/ -oo hotspot.o -eo hotspot.e -W 00:59 ',
+      ' -P ',project.ID,' -J ',x,'_tag_hotspot ',
       ' python /ifs/work/bergerm1/zhengy1/ACCESS-Pipeline-DEV/cwl_tools/hotspots/tag_hotspots.py ',
       ' -m ',results.dir,'/',x,'/',x,'_all_unique_calls.maf',
       ' -itxt /ifs/work/bergerm1/Innovation/Resources/Hotspots/hotspot-list-union-v1-v2_with_TERT.txt ',
@@ -96,10 +106,11 @@ compile_reads = function(
     # genotyping plasma samples -- plasma duplex&simplex, plasma normal, pooled plasma normal
     write.table(sample.sheet[,.(sample_id = Sample_Barcode,maf = paste0(results.dir,'/',x,'/',x,'_all_unique_calls.maf'),
                                 standard_bam,duplex_bam,simplex_bam)],
-                paste0(results.dir,'/',x,'/',x,'genotype_metadata.tsv'),sep = '\t',quote = F,row.names = F)
+                paste0(results.dir,'/',x,'/',x,'_genotype_metadata.tsv'),sep = '\t',quote = F,row.names = F)
     job.ids = system(paste0(
       'bsub -cwd ',results.dir,'/',x,' -W 12:00  -R "rusage[mem=8]"  -oo genotyping.o -eo genotyping.e ',
-      ' genotype_variants small_variants multiple-samples -i ',results.dir,'/',x,'/',x,'genotype_metadata.tsv',
+      ' -P ',project.ID,' -J ',x,'_genotype_variants ',
+      ' genotype_variants small_variants multiple-samples -i ',results.dir,'/',x,'/',x,'_genotype_metadata.tsv',
       ' -r ',fasta.path,' -g ',genotyper.path,' -v DEBUG '
     ),intern = T)
     job.ids = as.numeric(gsub('Job <|> is.*.$','',job.ids))
@@ -108,6 +119,7 @@ compile_reads = function(
   
   # Get base count multi sample in pooled normal ----------------------------
   # all all unique calls in entire cohort
+  print('Compiling reads in pooled samples')
   dir.create(paste0(results.dir,'/pooled'))
   all.all.unique.mafs = do.call(rbind,lapply(unique(master.ref$cmo_patient_id),function(x){
     fread(list.files(paste0(results.dir,'/',x),pattern = 'unique_calls.maf$',full.names = T))
@@ -122,25 +134,24 @@ compile_reads = function(
   pooled.sample.job.id = system(paste0(
     'bsub -cwd ',results.dir,'/pooled -W 12:00  -R "rusage[mem=8]" -oo genotyping.o -eo genotyping.e ',
     ' -w ',' \"',paste0(paste0('done(',unlist(all.fillout.id),')'),collapse = '&&'),'\" ',
+    ' -P ',project.ID,' -J pooled_genotype_variants ',
     ' genotype_variants small_variants multiple-samples -i ',results.dir,'/pooled/pooled_metadata.tsv',
     ' -r ',fasta.path,' -g ',genotyper.path,' -v DEBUG '
   ),intern = T)
   pooled.sample.job.id = as.numeric(gsub('Job <|> is.*.$','',pooled.sample.job.id))
-  print(pooled.sample.job.id)
-  
-  
-  # # checking if all jobs ran correctly --------------------------------------
-  # out.log.filenames = list.files(results.dir,pattern = '\\.o',recursive = T,full.names = T)
-  # out.log = unlist(lapply(out.log.filenames,function(x){
-  #   file.content = readLines(x)
-  #   job.status = gsub('^.*.cluster <solar. ','',file.content[which(grepl('Subject: ',file.content))])
-  # }))
-  # if(all(out.log == 'Done')){
-  #   print('Everything ran correctly')
-  # }
+  while(
+    !any(grepl('Done successfully',system(paste0('bjobs -l ',pooled.sample.job.id),intern = T)))
+  ){
+    Sys.sleep(120)
+  }
+  print('Compile reads done!')
 }
 
 # Executable -----------------------------------------------------------------------------------------------------------
+# Minimal columns for input mafs
+# 
+# Hugo_Symbol,Chromosome,Start_Position,End_Position,Tumor_Sample_Barcode,Variant_Classification,HGVSp_Short,Reference_Allele,Tumor_Seq_Allele2,D_t_alt_count_fragment
+
 suppressPackageStartupMessages({
   library(data.table)
   library(tidyr)
@@ -154,6 +165,8 @@ if (!interactive()) {
   parser=ArgumentParser()
   parser$add_argument('-m', '--masterref', type='character', help='File path to master reference file')
   parser$add_argument('-o', '--resultsdir', type='character', help='Output directory')
+  parser$add_argument('-pid', '--projectid', type='character',default = '', 
+                      help='Project ID for submitted jobs involved in this run')
   parser$add_argument('-pb', '--pooledbamdir', type='character',default = '/ifs/work/bergerm1/ACCESS-Projects/novaseq_curated_duplex_v2/', 
                       help='Directory for all pooled bams [default]')
   parser$add_argument('-fa', '--fastapath', type='character',default = '/work/access/production/resources/reference/current/Homo_sapiens_assembly19.fasta', 
@@ -170,6 +183,7 @@ if (!interactive()) {
   
   master.ref = args$masterref
   results.dir = args$resultsdir
+  project.ID = args$projectid
   pooled.bam.dir = args$pooledbamdir
   fasta.path = args$fastapath
   genotyper.path = args$genotyperpath
@@ -177,6 +191,12 @@ if (!interactive()) {
   mirror.bam.dir = args$mirrorbamdir
   dmp.key.path = args$dmpkeypath
   
+  
+  if(project.ID == ''){
+      project.ID = paste0(sample(c(0:9),size = 10,replace = T),collapse = '')
+  }
+  
+  print(paste0('Input parameters for run ',project.ID))
   print(master.ref)
   print(results.dir)
   print(pooled.bam.dir)
@@ -185,7 +205,6 @@ if (!interactive()) {
   print(dmp.dir)
   print(mirror.bam.dir)
   print(dmp.key.path)
-  
-  suppressWarnings(compile_reads(fread(master.ref),results.dir,pooled.bam.dir,fasta.path,genotyper.path,dmp.dir,mirror.bam.dir,dmp.key.path))
-  
+  suppressWarnings(compile_reads(fread(master.ref),results.dir,project.ID,pooled.bam.dir,fasta.path,genotyper.path,dmp.dir,mirror.bam.dir,dmp.key.path))
+  print('compile reads function finished')
 }
