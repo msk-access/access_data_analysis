db2e459a1dff9b0b8864ee280732c63726de7910
Ronak Shah <rons.shah@gmail.com>
2020-07-22 13:07:11 -0400

    Update plot_all_events.R

--- R/plot_all_events.R
+++ R/plot_all_events.R
@@ -227,7 +227,7 @@ plot_all_events <- function(
         x = Tumor_Sample_Barcode, y = ifelse(t_total_count == 0, 0, as.numeric(t_alt_count / t_total_count)),
         color = paste0(Hugo_Symbol, " ", ifelse(grepl("^p\\.", HGVSp_Short), HGVSp_Short, "")), shape = call_confidence
       ), size = 1.5) +
-      labs(title = x, x = "Time Point", y = "VAF(log10)") +
+      labs(title = x, x = "Time Point(weeks)", y = "log10(Variant Allele Frequency)") +
       scale_shape_manual(values = status_id, name = "Call Status") +
       scale_color_manual(values = getPalette(colourCount), name = "Alteration") +
       theme_minimal() +
@@ -247,7 +247,7 @@ plot_all_events <- function(
         x = Tumor_Sample_Barcode, y = ifelse(t_total_count == 0, 0, as.numeric(t_alt_count / t_total_count)),
         color = paste0(Hugo_Symbol, " ", ifelse(grepl("^p\\.", HGVSp_Short), HGVSp_Short, "")), shape = call_confidence
       ), size = 1.5) +
-      labs(title = x, x = "Time Point", y = "VAF") +
+      labs(title = x, x = "Time Point(weeks)", y = "Variant Allele Frequency") +
       scale_shape_manual(values = status_id, name = "Call Status") +
       scale_color_manual(values = getPalette(colourCount), name = "Alteration") +
       theme_minimal() +
@@ -272,18 +272,18 @@ plot_all_events <- function(
       getPalette <- colorRampPalette(brewer.pal(8, "Set2"))
       CNA.plot <- ggplot(tmp.cna) +
         geom_bar(aes(x = Tumor_Sample_Barcode, y = abs(fc), fill = paste0(Hugo_Symbol, "_", CNA)), position = "dodge", stat = "identity") +
-        labs(x = "Time Point", y = "Absolute fc") +
+        labs(x = "Time Point (weeks)", y = "Absolute fold-change") +
         scale_fill_manual(values = getPalette(colourCount), name = "Alteration") +
         theme_minimal() +
         scale_x_date(date_minor_breaks = "1 day", date_breaks = "1 week", date_labels = "%b %d") +
         theme(panel.grid.major = element_blank(), legend.position = "bottom", axis.text.x = element_text(angle = 45, hjust = 1, face = "bold"))
       print(CNA.plot)
 
-      pdf(paste0(output.dir, "/", x, "_all_events.pdf"), width = 10, height = 7)
+      pdf(paste0(output.dir, "/", x, "_all_events.pdf"), width = 16, height = 8)
       print(ggarrange(SNV.SV.plot.log, SNV.SV.plot.linear, CNA.plot, CNA.plot, ncol = 2, nrow = 2, heights = c(2, 2, 1, 1)))
       dev.off()
     } else {
-      pdf(paste0(output.dir, "/", x, "_all_events.pdf"), width = 10, height = 7)
+      pdf(paste0(output.dir, "/", x, "_all_events.pdf"), width = 16, height = 8)
       print(ggarrange(SNV.SV.plot.log, SNV.SV.plot.linear, ncol = 2, heights = c(2, 2)))
       dev.off()
     }
