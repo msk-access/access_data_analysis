d5b2f07dc3cfd20b6f4ad136762daca9808ae94d
Kanika Arora <arorak@selene.cbio.private>
2021-03-09 21:57:52 -0500

    Bugfixes: Sample_Barcode and Chromosomes in pooled normal set to as.character so that merging doesn't fail

--- R/filter_calls.R
+++ R/filter_calls.R
@@ -38,6 +38,7 @@ filter_calls = function(
     group_by(Hugo_Symbol,Chromosome,Start_Position,End_Position,Variant_Classification,Reference_Allele,Tumor_Seq_Allele2) %>%
     summarise(duplex_support_num = length(which(t_alt_count >= 2))) %>%
     filter(duplex_support_num > 0,.preserve = T) %>%
+    transmute(Hugo_Symbol, Chromosome=as.character(Chromosome), Start_Position, End_Position, Variant_Classification, Reference_Allele, Tumor_Seq_Allele2, duplex_support_num) %>%
     data.table()
 
   # for each patient produce the correct results ----------------------------
