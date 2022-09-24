6d8cfe0d360d3429fa85e860364957b46381bf82
Ronak Shah <rons.shah@gmail.com>
2022-09-24 14:32:24 -0400

    Update create_report.R

--- reports/create_report.R
+++ reports/create_report.R
@@ -47,7 +47,7 @@ input_text <- knitr::knit_expand(
 )
 tmp <- NULL
 if(args$keep_rmarkdown) {
-  tmp <- gsub(".html",".Rmd",args$output_file)
+  tmp <- gsub("\.html","\.Rmd",args$output_file)
 } else {
   tmp <- tempfile(fileext = ".Rmd")
 }
