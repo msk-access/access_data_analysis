fa4824873c1b3bc50cf617ceafa659947f8d529b
Ronak Shah <rons.shah@gmail.com>
2022-09-30 11:46:49 -0400

    Update get_cbioportal_variants.py

--- python/get_cbioportal_variants/get_cbioportal_variants.py
+++ python/get_cbioportal_variants/get_cbioportal_variants.py
@@ -3,8 +3,6 @@ from typing import List, Optional
 from bed_lookup import BedFile
 import typer
 import pandas as pd
-import csv
-import sys
 
 def main(
     maf: Path = typer.Option(
@@ -93,21 +91,9 @@ def main(
 
 # preprocessing
 def get_row(file):
-    maxInt = sys.maxsize
-    while True:
-        # decrease the maxInt value by factor 10 
-        # as long as the OverflowError occurs.
-        try:
-            csv.field_size_limit(maxInt)
-            break
-        except OverflowError:
-            maxInt = int(maxInt/10) 
     skipped = []
-    with open(file, "r") as csvfile:
-        reader = csv.reader(csvfile, delimiter="\t")
-        for i, row in enumerate(reader):
-            if row[0].strip()[:2] == "#":
-                skipped.append(i)
+    with open(file, "r") as csv_file:
+        skipped.extend(i for i, line in enumerate(csv_file) if line.startswith("#"))
     return skipped
 
 
