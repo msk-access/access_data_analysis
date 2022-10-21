9b17ceba537768ec40a08ea2ef1c44a7b3b90c05
Karthigayini Sivaprakasam <ksivapra@asu.edu>
2022-10-22 00:54:48 +0530

    Revert "Update filter_calls.R"
    This reverts commit bf47a9e333b44d60973f740f421c93a732935fca.

--- R/filter_calls.R
+++ R/filter_calls.R
@@ -1,10 +1,6 @@
-```R
 # library(data.table)
-
 # library(tidyr)
-
 # library(stringr)
-
 # library(dplyr)
 
 #' @export
@@ -13,25 +9,15 @@ filter_calls = function(
   CH.path = '/juno/work/access/production/resources/dmp_signedout_CH/current/signedout_CH.txt',
   criteria = 'stringent'
 ){
-
   # # test input section -----------------------------------------------------------
-
   # master.ref = fread('/juno/work/bergerm1/bergerlab/zhengy1/access_data_analysis/data/example_master_file.csv')
-
   # results.dir = paste0('/juno/work/bergerm1/MSK-ACCESS/ACCESS-Projects/test_access/access_data_analysis/output_042020/')
-
   # dmp.key.path = '/ifs/dmprequest/12-245/key.txt'
-
   # CH.calls = fread('/ifs/work/bergerm1/zhengy1/RET_all/Original_files/signedout_CH.txt')
-
   # # criteria <- 'permissive'
-
   # criteria <- 'stringent'
-
   #
-
   # criteria definition -----------------------------------------------------
-
   if(criteria == 'permissive'){
     hotspot.support <- 1
     non.hotspot.support <- 3
@@ -43,9 +29,7 @@ filter_calls = function(
   dir.create(paste0(results.dir,'/results_',criteria))
 
   # inputs ---------------------------------------------------------------
-
   # DMP.key <- fread(dmp.key.path)
-
   CH.calls = fread(CH.path)
   pooled.normal.mafs <-
     fread(paste0(results.dir,'/pooled/all_all_unique.maf')) %>%
@@ -58,48 +42,46 @@ filter_calls = function(
     data.table()
 
   # for each patient produce the correct results ----------------------------
-
   # x <- unique(master.ref$cmo_patient_id)[1]
-
   all.fillout.dim <- lapply(unique(master.ref$cmo_patient_id),function(x){
     print(paste0('Processing patient ',x))
 
     # Inputs and sanity checks ------------------------------------------------
     fillouts.filenames <- list.files(paste0(results.dir,'/',x,'/'),'ORG-STD_genotyped.maf|ORG-SIMPLEX-DUPLEX_genotyped.maf',full.names = T)
-    
+
     # compiling a sample sheet with duplex, simplex, normal, DMP tumor and DMP normal
     sample.sheet <- fread(paste0(results.dir,'/',x,'/',x,'_sample_sheet.tsv'))[,.(Sample_Barcode,cmo_patient_id,Sample_Type)]
     simplex.sample.sheet = sample.sheet[Sample_Type == 'duplex',.(Sample_Barcode,cmo_patient_id,Sample_Type = 'simplex')]
     sample.sheet = rbind(sample.sheet,simplex.sample.sheet) %>%
       mutate(column.names = paste0(Sample_Barcode,'___',Sample_Type)) %>%
       data.table()
-    
+
     # compiling different genotype files from step 1
     fillouts.dt <- do.call(rbind,lapply(fillouts.filenames,function(y){
       sample.name = gsub('.*./|-ORG.*.','',y)
       sample.type = unique(sample.sheet[Sample_Barcode == sample.name]$Sample_Type)
-    
+
       # t_alt_count,t_ref_count,t_depth these columns are useless, have to use duplex/simplex/standard columms
       maf.file <- fread(y) %>%
         select(-c(t_alt_count,t_ref_count)) %>%
         data.table()
-    
+
       if (nrow(maf.file) == 0) {
-    
+
         columns <- c(
           "Hugo_Symbol", "Tumor_Sample_Barcode", "Chromosome", "Start_Position",
           "End_Position", "Variant_Classification", "HGVSp_Short",
           "Reference_Allele", "Tumor_Seq_Allele2", "t_var_freq", "gnomAD_AF")
         df <- data.frame(matrix(ncol = length(columns), nrow = 0))
         colnames(df) <- columns
-    
+
         return(df)
       }
-    
+
       # fragment counts replacing actual allele counts
       if(grepl('SIMPLEX-DUPLEX_genotyped',y)){
         melt.id.vars = colnames(maf.file)[!grepl('fragment',colnames(maf.file))]
-    
+
         # get rid of simplex duplex aggregate columns
         maf.file %>%
           select(-c(contains('simplex_duplex'))) %>%
@@ -119,18 +101,18 @@ filter_calls = function(
           # swaping the t_alt_count(etc)_standard for t_alt_count(etc)
           mutate(t_alt_count = t_alt_count_standard,t_total_count = t_total_count_standard)
       }
-    
+
       maf.file = maf.file %>%
         mutate(t_var_freq = paste0(t_alt_count,'/',t_total_count,'(',round(t_alt_count/t_total_count,4),')')) %>%
         transmute(Hugo_Symbol,Tumor_Sample_Barcode,Chromosome = as.character(Chromosome),Start_Position,End_Position,Variant_Classification,
-                  HGVSp_Short=as.character(HGVSp_Short),Reference_Allele,Tumor_Seq_Allele2,t_var_freq,"gnomAD_AF") %>%
+                  HGVSp_Short=as.character(HGVSp_Short),Reference_Allele,Tumor_Seq_Allele2,t_var_freq,'gnomAD_AF') %>%
         data.table()
-    
+
       return(maf.file)
     })) %>%
       unique() %>%
       data.table()
-    
+
     # merging and melting -----------------------------------------------------
     hotspot.maf <- fread(paste0(results.dir,'/',x,'/',x,'_all_unique_calls_hotspots.maf')) %>%
       rowwise() %>%
@@ -138,7 +120,7 @@ filter_calls = function(
                 # HGVSp_Short,
                 Reference_Allele,Tumor_Seq_Allele2,Hotspot = ifelse(hotspot_whitelist,'Hotspot',NA)) %>%
       data.table()
-    
+
     dmp.maf <- fread(paste0(results.dir,'/',x,'/',x,'_impact_calls.maf')) %>%
       transmute(Hugo_Symbol,Chromosome = as.character(Chromosome),Start_Position,End_Position,Variant_Classification,
                 # HGVSp_Short,
@@ -146,7 +128,7 @@ filter_calls = function(
       mutate(DMP = 'Signed out') %>%
       unique() %>%
       data.table()
-    
+
     if((nrow(dmp.maf) > 0) && (nrow(fillouts.dt) > 0)){
         fillouts.dt <- fillouts.dt %>%
           dcast.data.table(Hugo_Symbol + Chromosome + Start_Position + End_Position + Variant_Classification +
@@ -187,39 +169,39 @@ filter_calls = function(
         mutate(DMP = NA) %>%
         data.table()
     } else {
-    
+
       print(paste0("Found no tumor or DMP mutations for ", x, ". Writing an empty data.frame to CSV."))
-    
+
       # if fillouts.dt has no data, then add the needed columns with no data
       fillouts.dt[,c("DMP", "Hotspot", "duplex_support_num", "call_confidence", "CH") := NA]
-    
+
       fillouts.dt <- fillouts.dt %>% select(
         Hugo_Symbol,Chromosome,Start_Position,End_Position,
         Variant_Classification,HGVSp_Short,Reference_Allele,Tumor_Seq_Allele2,
         gnomAD_AF,Hotspot,DMP,CH,duplex_support_num,call_confidence,sort(everything()))
-    
+
       write.csv(
         fillouts.dt,
         paste0(results.dir,'/results_',criteria,'/',x,'_SNV_table.csv'),
         row.names = F)
-    
+
       return()
     }
-    
+
     # Interesting cases where DMP signed out calls are artifacets
     if(any(!is.na(fillouts.dt$DMP) & !is.na(fillouts.dt$duplex_support_num))){
       print(paste0('Look at ',x,' for DMP signed out plasma artifacts...'))
     }
-    
+
     # germline filtering for matched and unmatched ----------------------------
     plasma.samples <- sample.sheet[Sample_Type %in% c('duplex')]$column.names
     normal.samples <- sample.sheet[Sample_Type %in% c('unfilterednormal','normal_DMP')]$column.names
     fillouts.dt[,c(
       paste0(plasma.samples,'.called')
       # paste0(gsub('duplex','simplex',plasma.samples),'.called')
-    
+
     ) := 'Not Called']
-    
+
     # preliminary calling
     # tmp.col.name <- plasma.samples[1]
     lapply(plasma.samples,function(tmp.col.name){
@@ -237,7 +219,7 @@ filter_calls = function(
       # c(eval(paste0(tmp.col.name,'.called')),eval(paste0(gsub('duplex','simplex',tmp.col.name),'.called'))) := list('Called','Called')]
       # print(table(fillouts.dt[,get(paste0(tmp.col.name,'.called'))]))
     })
-    
+
     if(all(!c('unfilterednormal','normal_DMP') %in% sample.sheet$Sample_Type)){
       tmp.col.name <- plasma.samples[1]
       lapply(plasma.samples,function(tmp.col.name){
@@ -257,7 +239,7 @@ filter_calls = function(
         })
       })
     }
-    
+
     # final processing --------------------------------------------------------
     # Save only the useful column
     #print(fillouts.dt)
@@ -275,7 +257,7 @@ filter_calls = function(
       )]
       fillouts.dt[,c(eval(gsub('duplex','simplex',tmp.col.name)),eval(tmp.col.name)):= list(NULL,NULL)]
     })
-    
+
     fillouts.dt <- fillouts.dt[,order(colnames(fillouts.dt)),with = F] %>%
       # filter for artifacts
       mutate(call_confidence = case_when(
@@ -290,9 +272,8 @@ filter_calls = function(
       mutate(CH = ifelse(is.na(CH),'No','Yes')) %>%
       select(Hugo_Symbol,Chromosome,Start_Position,End_Position,Variant_Classification,HGVSp_Short,Reference_Allele,Tumor_Seq_Allele2,
             gnomAD_AF,Hotspot,DMP,CH,duplex_support_num,call_confidence,sort(everything()))
-    
-    write.csv(fillouts.dt,paste0(results.dir,'/results_',criteria,'/',x,'_SNV_table.csv'),row.names = F)
 
+    write.csv(fillouts.dt,paste0(results.dir,'/results_',criteria,'/',x,'_SNV_table.csv'),row.names = F)
   })
 
   if(all(unlist(all.fillout.dim))){
@@ -302,7 +283,6 @@ filter_calls = function(
 }
 
 # Executable -----------------------------------------------------------------------------------------------------------
-
 suppressPackageStartupMessages({
   library(data.table)
   library(tidyr)
@@ -337,5 +317,3 @@ if (!interactive()) {
   suppressWarnings(filter_calls(fread(master.ref),results.dir,chlist,criteria))
 
 }
-```
-
