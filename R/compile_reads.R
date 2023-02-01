3eba1791e1409bed71e99d64e86abeec4af2f49b
Ronak Shah <rons.shah@gmail.com>
2023-02-01 14:47:43 -0500

    adding access samples to genotype

--- R/compile_reads.R
+++ R/compile_reads.R
@@ -104,12 +104,12 @@ compile_reads <- function(master.ref,
           DMP.key[grepl(paste0(dmp_id, "-(T|N)..-IH."), V1)]$V2
         all.dmp.bam.ids.XS <-
           access.key[grepl(paste0(dmp_id, "-(T|N)..-XS."), V1)]$V2
-        cat(all.dmp.bam.ids.XS)
+        print(all.dmp.bam.ids.XS)
         all.dmp.bam.ids.XS <-
           gsub("-standard|-unfilter|-simplex|-duplex",
                "",
                access.key[grepl(paste0(dmp_id, "-(T|N)..-XS."), V1)]$V2)
-        cat(all.dmp.bam.ids.XS)
+        print(all.dmp.bam.ids.XS)
         all.dmp.bam.ids <-
           c(all.dmp.bam.ids.IM,
             all.dmp.bam.ids.IH)
