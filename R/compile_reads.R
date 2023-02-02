57500c189873bbedbe5e76b218691759411fb05c
Ronak Shah <rons.shah@gmail.com>
2023-02-01 22:16:24 -0500

    Update compile_reads.R

--- R/compile_reads.R
+++ R/compile_reads.R
@@ -40,27 +40,19 @@ compile_reads <- function(master.ref,
 
   # data from DMP -----------------------------------------------------------
   DMP.key <- fread(dmp.key.path)
-  if (any(
-    !master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id %in% gsub("-T..-IH.|-T..-IM.", "", DMP.key[grepl("IH|IM", V1)]$V1)
-  )) {
+  if (any(!master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id %in% gsub("-T..-IH.|-T..-IM.", "", DMP.key[grepl("IH|IM", V1)]$V1))) {
     message(paste0(
       "These DMP IDs are not found in DMP key file: ",
-      paste0(master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id[which(
-        !master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id %in%
-          gsub("-T..-IH.|-T..-IM.", "", DMP.key[grepl("IH|IM", V1)]$V1)
-      )], collapse = " ,")
+      paste0(master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id[which(!master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id %in%
+                                                                             gsub("-T..-IH.|-T..-IM.", "", DMP.key[grepl("IH|IM", V1)]$V1))], collapse = " ,")
     ))
   }
   access.key <- fread(access.key.path)
-  if (any(
-    !master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id %in% gsub("-T..-XS.", "", access.key[grepl("XS", V1)]$V1)
-  )) {
+  if (any(!master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id %in% gsub("-T..-XS.", "", access.key[grepl("XS", V1)]$V1))) {
     message(paste0(
       "These DMP IDs are not found in DMP ACCESS key file: ",
-      paste0(master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id[which(
-        !master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id %in%
-          gsub("-T..-XS.", "", access.key[grepl("XS", V1)]$V1)
-      )], collapse = " ,")
+      paste0(master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id[which(!master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id %in%
+                                                                             gsub("-T..-XS.", "", access.key[grepl("XS", V1)]$V1))], collapse = " ,")
     ))
   }
   DMP.maf <-
@@ -68,7 +60,7 @@ compile_reads <- function(master.ref,
     filter(Mutation_Status != "GERMLINE") %>%
     data.table()
   DMP.RET.maf <-
-    DMP.maf[grepl(paste0(unique(master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id), collapse = "|"), Tumor_Sample_Barcode),]
+    DMP.maf[grepl(paste0(unique(master.ref[grepl("^P-", dmp_patient_id)]$dmp_patient_id), collapse = "|"), Tumor_Sample_Barcode), ]
 
   # Pooled normal samples ---------------------------------------------------
   pooled.bams <-
@@ -434,7 +426,7 @@ compile_reads <- function(master.ref,
           HGVSp_Short,
           Reference_Allele,
           Tumor_Seq_Allele2
-        )])),] %>%
+        )])), ] %>%
         mutate(
           t_ref_count = 0,
           t_alt_count = 0,
@@ -559,7 +551,7 @@ compile_reads <- function(master.ref,
       HGVSp_Short,
       Reference_Allele,
       Tumor_Seq_Allele2
-    )]), ]
+    )]),]
   write.table(
     all.all.unique.mafs,
     paste0(results.dir, "/pooled/all_all_unique.maf"),
