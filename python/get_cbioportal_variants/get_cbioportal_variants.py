409322908d2edffcb3055ed0292929edcd12ffc3
Ronak Shah <rons.shah@gmail.com>
2023-02-22 11:38:52 -0500

    Update get_cbioportal_variants.py

--- python/get_cbioportal_variants/get_cbioportal_variants.py
+++ python/get_cbioportal_variants/get_cbioportal_variants.py
@@ -51,12 +51,14 @@ def subset_cna(
         if not sid:
             typer.echo("Identifiers were not provided via command line as well")
             raise typer.Abort()
-        
-    cna_df = read_tsv(sv)
-    ids_to_subset = read_ids(sid,ids)
-    subset_tsv = filter_by_column(ids_to_subset,cna_df)
+
+    cna_df = read_tsv(cna)
+    ids_to_subset = read_ids(sid, ids)
+    all_headers = ids_to_subset.insert(0, "Hugo_Symbol")
+    subset_tsv = filter_by_columns(all_headers, cna_df)
     subset_tsv.drop_duplicates().to_csv(output_file, sep="\t", index=False)
-    
+
+
 @app.command()
 def subset_sv(
     sv: Path = typer.Option(
@@ -87,7 +89,7 @@ def subset_sv(
         "-n",
         help="Name of the output file",
     ),
-        col_name: str = typer.Option(
+    col_name: str = typer.Option(
         "Sample_ID",
         "--cname",
         "-c",
@@ -109,10 +111,11 @@ def subset_sv(
             raise typer.Abort()
 
     sv_df = read_tsv(sv)
-    ids_to_subset = read_ids(sid,ids)
-    subset_tsv = filter_by_row(ids_to_subset,sv_df,col_name)
+    ids_to_subset = read_ids(sid, ids)
+    subset_tsv = filter_by_rows(ids_to_subset, sv_df, col_name)
     subset_tsv.drop_duplicates().to_csv(output_file, sep="\t", index=False)
-  
+
+
 @app.command()
 def subset_maf(
     maf: Path = typer.Option(
@@ -177,59 +180,120 @@ def subset_maf(
         if not sid:
             typer.echo("Identifiers were not provided via command line as well")
             raise typer.Abort()
-        
+
     maf_df = read_tsv(maf)
-    ids_to_subset = read_ids(sid,ids)
-    subset_maf = filter_by_row(ids_to_subset,maf_df,col_name)
+    ids_to_subset = read_ids(sid, ids)
+    subset_maf = filter_by_rows(ids_to_subset, maf_df, col_name)
     bed_obj = read_bed(bed)
-    maf_with_covered_tag = check_if_covered (bed_obj,subset_maf)
+    maf_with_covered_tag = check_if_covered(bed_obj, subset_maf)
     maf_with_covered_tag["Chromosome"] = maf_with_covered_tag["Chromosome"].apply(str)
     maf_with_covered_tag.drop_duplicates().to_csv(output_file, sep="\t", index=False)
 
+
 def read_tsv(maf):
-    # Read maf files
+    """Read a tsv file
+
+    Args:
+        maf (File): Input MAF/tsv like format file
+
+    Returns:
+        data_frame: Output a data frame containing the MAF/tsv
+    """
     skip = get_row(maf)
     return pd.read_csv(maf, sep="\t", skiprows=skip, low_memory=False)
 
-def read_ids(sid,ids):
-    # Read Identifiers
+
+def read_ids(sid, ids):
+    """make a list of ids
+
+    Args:
+        sid (tuple): Multiple ids as tuple
+        ids (File): File containing multiple ids
+
+    Returns:
+        list: List containing all ids
+    """
     if not sid:
         with open(ids) as file:
             sid = file.read().splitlines()[1:]
     return sid
 
-def filter_by_row(sid,tsv_df):
-    # filter for ids
-    ns = sid.insert(0,"Hugo_Symbol")
-    subset_tsv = tsv_df.filter(items=ns)
+
+def filter_by_columns(sid, tsv_df):
+    """Filter data by columns
+
+    Args:
+        sid (list): list of columns to subset over
+        tsv_df (data_frame): data_frame to subset from
+
+    Returns:
+        data_frame: A copy of the subset of the data_frame
+    """
+    subset_tsv = tsv_df.filter(items=sid)
     return subset_tsv.copy(deep=True)
 
-def filter_by_column(sid,tsv_df,col_name):
-    # filter for ids
+
+def filter_by_rows(sid, tsv_df, col_name):
+    """Filter the data by rows
+
+    Args:
+        sid (list): list of row names to subset over
+        tsv_df (data_frame): data_frame to subset from
+        col_name (string): name of the column to filter using names in the sid
+
+    Returns:
+        data_frame: A copy of the subset of the data_frame
+    """
     ns = set(sid)
     pattern = "|".join([f"\b{i}\b" for i in ns])
     result = tsv_df[tsv_df[col_name].str.contains(pattern, regex=True)]
     return result.copy(deep=True)
 
+
 def read_bed(bed):
+    """Read BED file using bed_lookup
+
+    Args:
+        bed (file): File ins BED format to read
+
+    Returns:
+        object : bed file object to use for filtering
+    """
     return BedFile(bed.as_posix())
 
+
 def check_if_covered(bedObj, mafObj):
+    """Function to check if a variant is covered in a given bed file
+
+    Args:
+        bedObj (object): BED file object to check coverage
+        mafObj (data_frame): data frame to check coverage against coordinates using column 'Chromosome' and position column is 'Start_Position'
+
+    Returns:
+        data_frame: _description_
+    """
     # Our chromosome column is 'Chromosome' and position column is 'Start_Position'.
-    mafObj["covered"] = bedObj.lookup_df(
-        mafObj, "Chromosome", "Start_Position"
-    )
+    mafObj["covered"] = bedObj.lookup_df(mafObj, "Chromosome", "Start_Position")
     mafObj.loc[mafObj["covered"].notnull(), "covered"] = "yes"
     mafObj.loc[mafObj["covered"].notna(), "covered"] = "yes"
     mafObj.loc[mafObj["covered"].isnull(), "covered"] = "no"
     mafObj.loc[mafObj["covered"].isna(), "covered"] = "no"
     return mafObj
 
+
 # preprocessing
-def get_row(file):
+def get_row(tsv_file):
+    """Function to skip rows
+
+    Args:
+        tsv_file (file): file to be read
+
+    Returns:
+        list: lines to be skipped
+    """
     skipped = []
-    with open(file, "r") as csv_file:
-        skipped.extend(i for i, line in enumerate(csv_file) if line.startswith("#"))
+    with open(tsv_file, "r") as FH:
+        skipped.extend(i for i, line in enumerate(FH) if line.startswith("#"))
     return skipped
 
 
