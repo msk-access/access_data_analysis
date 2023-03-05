34f41ff2271d573361965929b9b99b2ec1a2f5a4
Ronak Shah <rons.shah@gmail.com>
2023-03-05 15:32:08 -0500

    Update run_cmd.py

--- python/run_create_report/modules/run_cmd.py
+++ python/run_create_report/modules/run_cmd.py
@@ -3,13 +3,14 @@ import subprocess
 import shlex
 from rich import print
 
+
 def run_cmd(cmd):
     """Given a system command run it using subprocess
 
     Args:
         cmd (str): System command to be run as a string
     """
-    print("\nrun_cmd:Command:",cmd,"\n")
+    print("\nrun_cmd:Command:", cmd, "\n")
     out = subprocess.Popen(
         (cmd),
         stdin=subprocess.PIPE,
@@ -20,9 +21,9 @@ def run_cmd(cmd):
     out.wait()
     stdout, stderr = out.communicate()
     if stderr is None:
-        print("run_cmd:stdout:\n",stdout)
+        print("run_cmd:stdout:\n", stdout)
     else:
-        print("run_cmd:stderr:\n",stderr)
+        print("run_cmd:stderr:\n", stderr)
     return out
 
 
